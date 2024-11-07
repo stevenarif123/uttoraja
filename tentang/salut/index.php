243bd2101731f9fec3b539967d344ab22e14c2fd
Steven Arif <stevenarif@gmail.com>
2024-11-07 21:46:48 +0800

    tambahan sedikit

--- tentang/salut/index.php
+++ tentang/salut/index.php
@@ -3,14 +3,14 @@
   <head>
     <meta charset="utf-8" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <title>TENTANG SALUT</title>
+    <title>TENTANG UNIVERSITAS TERBUKA</title>
     <meta name="description" content="" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     <link
       rel="shortcut icon"
       type="image/x-icon"
-      href="../assets/img/favicon.png"
+      href="../../assets/img/favicon.png"
     />
     <!-- Place favicon.ico in the root directory -->
 
@@ -19,25 +19,25 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 
     <!-- CSS here -->
-    <link rel="stylesheet" href="../assets/css/01-bootstrap.min.css" />
+    <link rel="stylesheet" href="../../assets/css/01-bootstrap.min.css" />
     <link rel="styles
-    heet" href="../assets/css/02-all.min.css" />
-    <link rel="stylesheet" href="../assets/css/03-jquery.magnific-popup.css" />
+    heet" href="../../assets/css/02-all.min.css" />
+    <link rel="stylesheet" href="../../assets/css/03-jquery.magnific-popup.css" />
     <!-- <link rel="stylesheet" href="assets/css/04-nice-select.css" /> -->
-    <link rel="stylesheet" href="../assets/css/05-odometer.css" />
-    <link rel="stylesheet" href="../assets/css/06-swiper.min.css" />
-    <link rel="stylesheet" href="../assets/css/07-animate.min.css" />
-    <link rel="stylesheet" href="../assets/css/08-custom-animate.css" />
-    <link rel="stylesheet" href="../assets/css/09-slick.css" />
-    <link rel="stylesheet" href="../assets/css/10-icomoon.css" />
-    <link rel="stylesheet" href="../assets/vendor/custom-animate/custom-animate.css"/>
-    <link rel="stylesheet" href="../assets/vendor/jarallax/jarallax.css" />
-    <link rel="stylesheet" href="../assets/vendor/odometer/odometer.min.css" />
-    <link rel="stylesheet" href="../assets/fonts/gilroy/stylesheet.css" />
+    <link rel="stylesheet" href="../../assets/css/05-odometer.css" />
+    <link rel="stylesheet" href="../../assets/css/06-swiper.min.css" />
+    <link rel="stylesheet" href="../../assets/css/07-animate.min.css" />
+    <link rel="stylesheet" href="../../assets/css/08-custom-animate.css" />
+    <link rel="stylesheet" href="../../assets/css/09-slick.css" />
+    <link rel="stylesheet" href="../../assets/css/10-icomoon.css" />
+    <link rel="stylesheet" href="../../assets/vendor/custom-animate/custom-animate.css"/>
+    <link rel="stylesheet" href="../../assets/vendor/jarallax/jarallax.css" />
+    <link rel="stylesheet" href="../../assets/vendor/odometer/odometer.min.css" />
+    <link rel="stylesheet" href="../../assets/fonts/gilroy/stylesheet.css" />
 
-    <link rel="stylesheet" href="../assets/css/style.css" />
-    <link rel="stylesheet" href="../assets/css/color1.css" />
-    <link rel="stylesheet" href="../assets/css/responsive.css" />
+    <link rel="stylesheet" href="../../assets/css/style.css" />
+    <link rel="stylesheet" href="../../assets/css/color1.css" />
+    <link rel="stylesheet" href="../../assets/css/responsive.css" />
   </head>
 
   <body class="body-gray-bg">
@@ -116,8 +116,8 @@
               <div class="main-header-one__bottom-inner">
                 <div class="main-header-one__bottom-left">
                   <div class="logo-box-one">
-                    <a href="../index.html">
-                      <img src="../assets/img/resource/logo.png" alt="Logo" />
+                    <a href="../../index.html">
+                      <img src="../../assets/img/resource/logo.png" alt="Logo" />
                     </a>
                   </div>
                 </div>
@@ -131,7 +131,7 @@
                       <nav class="menu-nav">
                         <div class="navbar-wrap main-menu">
                           <ul class="navigation">
-                            <li><a href="../index.html">Home</a></li>
+                            <li><a href="../../index.html">Home</a></li>
                             <li class="menu-item-has-children">
                               <a href="#">Aplikasi UT</a>
                               <ul class="sub-menu">
@@ -163,22 +163,22 @@
                             <li class="menu-item-has-children">
                               <a href="#">Layanan</a>
                               <ul class="sub-menu">
-                                <li><a href="../informasi.php">Informasi Akademik</a></li>
-                                <li><a href="../administrasi/">Administrasi Akademik</a></li>
-                                <li><a href="../kegiatan.php">Kegiatan Akademik</a></li>
-                                <li><a href="../modul/">Pengambilan Modul</a></li>
-                                <li><a href="../legalisir/">Legalisir Ijazah</a></li>
-                                <li><a href="../suratketerangan/">Surat Keterangan</a></li>
+                                <li><a href="../../informasi.php">Informasi Akademik</a></li>
+                                <li><a href="../../administrasi/">Administrasi Akademik</a></li>
+                                <li><a href="../../kegiatan.php">Kegiatan Akademik</a></li>
+                                <li><a href="../../modul/">Pengambilan Modul</a></li>
+                                <li><a href="../../legalisir/">Legalisir Ijazah</a></li>
+                                <li><a href="../../suratketerangan/">Surat Keterangan</a></li>
                               </ul>
                             </li>
-                            <li><a href="../galeri/">Galeri</a></li>
+                            <li><a href="../../galeri/">Galeri</a></li>
                             <li class="menu-item-has-children">
                               <a href="#">Tentang</a>
                               <ul class="sub-menu">
-                                <li><a href="../tentang/tentangut.php">Universitas Terbuka</a></li>
-                                <li><a href="../tentang/tentangsalut.php/">SALUT</a></li>
-                                <li><a href="../tentang/saluttator.php">SALUT Tana Toraja</a></li>
-                                <li><a href="../tentang/kepalasalut.php">Pesan Kepala SALUT</a></li>
+                                <li><a href="../../tentang/tentangut.php">Universitas Terbuka</a></li>
+                                <li><a href="../../tentang/tentangsalut.php/">SALUT</a></li>
+                                <li><a href="../../tentang/saluttator.php">SALUT Tana Toraja</a></li>
+                                <li><a href="../../tentang/kepalasalut.php">Pesan Kepala SALUT</a></li>
                               </ul>
                             </li>
                           </ul>
@@ -256,13 +256,13 @@
         </div>
         <div class="container">
           <div class="page-header__inner">
-            <h2>TENTANG SENTRA LAYANAN UNIVERSITAS TERBUKA</h2>:
+            <h2>TENTANG UNIVERSITAS TERBUKA</h2>
             <ul class="thm-breadcrumb">
               <li>
                 <a href="index.html"><span class="fa fa-home"></span> Home</a>
               </li>
               <li><i class="icon-right-arrow-angle"></i></li>
-              <li class="color-base"> Tentang Sentra Layanan Universitas Terbuka</li>
+              <li class="color-base"> Tentang Universitas Terbuka</li>
             </ul>
           </div>
         </div>
@@ -342,16 +342,16 @@
                     <div class="single-footer-widget-box single-footer-widget__links single-footer-widget__links-style2">
                       <ul class="clearfix">
                         <li>
-                          <p><a href="../tentang/tentangut.php">Tentang UT</a></p>
+                          <p><a href="../../tentang/tentangut.php">Tentang UT</a></p>
                         </li>
                         <li>
-                          <p><a href="../layanan/informasi.php">Informasi Akademik</a></p>
+                          <p><a href="../../layanan/informasi.php">Informasi Akademik</a></p>
                         </li>
                         <li>
-                          <p><a href="../administrasi/">Administrasi</a></p>
+                          <p><a href="../../administrasi/">Administrasi</a></p>
                         </li>
                         <li>
-                          <p><a href="../tentang/kepalasalut.php">Sapaan dari Kepala SALUT</a></p>
+                          <p><a href="../../tentang/kepalasalut.php">Sapaan dari Kepala SALUT</a></p>
                         </li>
                       </ul>
                     </div>
@@ -369,32 +369,32 @@
                       <ul class="clearfix">
                         <li>
                           <p>
-                            <a href="../informasi.php">Informasi Akademik</a>
+                            <a href="../../informasi.php">Informasi Akademik</a>
                           </p>
                         </li>
                         <li>
                           <p>
-                            <a href="../administrasi">Administrasi Akademik</a>
+                            <a href="../../administrasi">Administrasi Akademik</a>
                           </p>
                         </li>
                         <li>
                           <p>
-                            <a href="../kegiatan.php">Kegiatan</a>
+                            <a href="../../kegiatan.php">Kegiatan</a>
                           </p>
                         </li>
                         <li>
                           <p>
-                            <a href="../modul/">Pengambilan Modul</a>
+                            <a href="../../modul/">Pengambilan Modul</a>
                           </p>
                         </li>
                         <li>
                           <p>
-                            <a href="../suratketerangan">Surat Keterangan</a>
+                            <a href="../../suratketerangan">Surat Keterangan</a>
                           </p>
                         </li>
                         <li>
                           <p>
-                            <a href="../legalisir">Legalisir Ijazah</a>
+                            <a href="../../legalisir">Legalisir Ijazah</a>
                           </p>
                         </li>
                       </ul>
@@ -455,23 +455,23 @@
     <!-- Scroll-top-end-->
 
     <!-- JS here -->
-    <script src="../assets/js/jquery-3.6.0.min.js"></script>
-    <script src="../assets/js/01-ajax-form.js"></script>
-    <script src="../assets/js/02-bootstrap.min.js"></script>
-    <script src="../assets/js/03-jquery.appear.js"></script>
-    <script src="../assets/js/04-swiper.min.js"></script>
-    <script src="../assets/js/05-jquery.odometer.min.js"></script>
-    <script src="../assets/js/06-jquery.magnific-popup.min.js"></script>
+    <script src="../../assets/js/jquery-3.6.0.min.js"></script>
+    <script src="../../assets/js/01-ajax-form.js"></script>
+    <script src="../../assets/js/02-bootstrap.min.js"></script>
+    <script src="../../assets/js/03-jquery.appear.js"></script>
+    <script src="../../assets/js/04-swiper.min.js"></script>
+    <script src="../../assets/js/05-jquery.odometer.min.js"></script>
+    <script src="../../assets/js/06-jquery.magnific-popup.min.js"></script>
     <!-- <script src="assets/js/07-jquery.nice-select.min.js"></script> -->
-    <script src="../assets/js/08-slick.min.js"></script>
-    <script src="../assets/js/09-wow.min.js"></script>
-    <script src="../assets/js/10-jquery.circleType.js"></script>
-    <script src="../assets/js/11-jquery.lettering.min.js"></script>
-    <script src="../assets/js/12-TweenMax.min.js"></script>
-    <script src="../assets/vendor/jarallax/jarallax.min.js"></script>
-    <script src="../assets/vendor/marquee/marquee.min.js"></script>
-    <script src="../assets/vendor/odometer/odometer.min.js"></script>
+    <script src="../../assets/js/08-slick.min.js"></script>
+    <script src="../../assets/js/09-wow.min.js"></script>
+    <script src="../../assets/js/10-jquery.circleType.js"></script>
+    <script src="../../assets/js/11-jquery.lettering.min.js"></script>
+    <script src="../../assets/js/12-TweenMax.min.js"></script>
+    <script src="../../assets/vendor/jarallax/jarallax.min.js"></script>
+    <script src="../../assets/vendor/marquee/marquee.min.js"></script>
+    <script src="../../assets/vendor/odometer/odometer.min.js"></script>
 
-    <script src="../assets/js/main.js"></script>
+    <script src="../../assets/js/main.js"></script>
   </body>
 </html>
